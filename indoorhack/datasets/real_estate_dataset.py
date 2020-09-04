4b92db11baf835cb7d9a726ecfdaef79a1721826
Filip Finfando <filip.finfando@gmail.com>
2020-09-04 21:25:45 +0200

    clean datasets

--- indoorhack/datasets/real_estate_dataset.py
+++ indoorhack/datasets/real_estate_dataset.py
@@ -7,40 +7,38 @@ from torch.utils.data import Dataset
 from torchvision.datasets.folder import default_loader
 
 
-class ScanDataset(Dataset):
-    def __init__(self, path: str, meta=None, loader=None, transform=None, target_transform=None, use_label_encoding=False):
+class RealEstateDataset(Dataset):
+    def __init__(self, path: str, meta=None, loader=None, transform=None,
+                 use_label_encoding=False):
         self.path = Path(path)
         self.transform = transform
-        self.target_transform = target_transform
         self.le = None
-        
+
         if loader:
             self.loader = loader
         else:
             self.loader = default_loader
 
-        path2meta = lambda x: (tuple(x[-2].split("_")) + (x[-1].split(".")[0],))
-        meta2path = lambda sce, sca, seq: self.path / "_".join([sce, sca]) / (seq + ".jpg")
+        path2meta = lambda x: (x[-3], x[-2], x[-1])
+        meta2path = lambda apartment_id, scene_id, image_id: self.path / apartment_id / scene_id / image_id
 
         if path and not meta:
             self.images = [i for i in tqdm(Path(self.path).glob("**/*.jpg"))]
             self.meta = [path2meta(i.parts) for i in self.images]
         elif meta:
             self.meta = meta
-            self.images = [meta2path(sce, sca, seq) for sce, sca, seq in tqdm(meta)]
+            self.images = [meta2path(a, s, i) for a, s, i in tqdm(meta)]
 
         if use_label_encoding:
-            scene_id, _, _ = zip(*self.meta)
+            apartment_id, scene_id, _ = zip(*self.meta)
             self.le = LabelEncoder()
-            self.le.fit(scene_id)
+            self.le.fit("_".join(apartment_id, scene_id))
 
     def __getitem__(self, idx):
         path, meta = self.images[idx], self.meta[idx]
         sample = self.loader(path)
         if self.transform is not None:
             sample = self.transform(sample)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
 
         if self.le:
             return sample, self.le.transform([meta[0]])
