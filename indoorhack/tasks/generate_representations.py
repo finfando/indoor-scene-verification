d65e747131e19865f016ae09a70c4dc32cb8ceb6
Filip Finfando <filip.finfando@gmail.com>
2020-10-15 19:30:00 +0000

    generate less repr for validation

--- indoorhack/tasks/generate_representations.py
+++ indoorhack/tasks/generate_representations.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import click
+import numpy as np
 import pandas as pd
 from indoorhack.tasks.utils import get_dataset, get_model, get_loader, get_transformer, get_experiment
 from config.env import SCAN_DATA_PATH
@@ -8,7 +9,8 @@ from config.env import SCAN_DATA_PATH
 @click.option("--dataset_type", type=click.Choice(["scan", "real_estate"]), required=True)
 @click.option("--model_type", type=click.Choice(["hash", "orb", "netvlad", "facenet", "indoorhack-v1", "indoorhack-v2", "indoorhack-v21"]), required=True)
 @click.option("--dataset_name", help="Name of dataset.", required=True)
-def generate_representations(dataset_type, model_type, dataset_name):
+@click.option("--val_only", help="Generate only representations of images in val dataset", is_flag=True)
+def generate_representations(dataset_type, model_type, dataset_name, val_only):
     if dataset_type == "scan":
         dataset_path = SCAN_DATA_PATH / dataset_name
     else:
@@ -24,6 +26,17 @@ def generate_representations(dataset_type, model_type, dataset_name):
     loader = get_loader(model_type)
     transform = get_transformer(model_type)
     meta = pd.read_pickle(meta_path)
+    if val_only and dataset_type == "scan":
+        path10 = Path("/home/filip/indoor_scene_verification/data/scan/scannet_val_10_X.npy")
+        path20 = Path("/home/filip/indoor_scene_verification/data/scan/scannet_val_20_X.npy")
+        path30 = Path("/home/filip/indoor_scene_verification/data/scan/scannet_val_30_X.npy")
+        data10 = np.load(path10)
+        data20 = np.load(path20)
+        data30 = np.load(path30)
+        data = np.vstack([data10, data20, data30]).flatten()
+        uq_indices = np.unique(data)
+        meta_arr = np.array(meta)[uq_indices]
+        meta = list(map(tuple, meta_arr))
     dataset = get_dataset(
         dataset_type, path=dataset_path, meta=meta, loader=loader, transform=transform
     )
