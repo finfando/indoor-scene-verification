82427b5ee0b0557307693d9c6297f14a527e7468
Filip Finfando <filip.finfando@gmail.com>
2020-10-18 17:07:43 +0000

    new colors

--- config/plot.py
+++ config/plot.py
@@ -1,11 +1,26 @@
 EXPERIMENT_MAP = {
-    "hash": {"name": "pHash", "color": "#1f77b4"},
-    "orb": {"name": "ORB", "color": "#ff7f0e"},
-    "netvlad": {"name": "NetVLAD", "color": "#2ca02c"},
-    "facenet": {"name": "FaceNet", "color": "#d62728"},
-    "indoorhack-v1": {"name": "IndoorHack-v1", "color": "#9467bd"},
-    "indoorhack-v2": {"name": "IndoorHack-v2", "color": "#8c564b"},
-    "indoorhack-v21": {"name": "IndoorHack-v21", "color": "#e377c2"},
+    "hash": {"name": "pHash", "color": "#1f77b4", "linestyle": "solid"},
+    "orb": {"name": "ORB", "color": "#ff7f0e", "linestyle": "solid"},
+    "netvlad": {"name": "NetVLAD", "color": "#2ca02c", "linestyle": "solid"},
+    "facenet": {"name": "FaceNet", "color": "#d62728", "linestyle": "solid"},
+    # "indoorhack-v1": {"name": "IndoorHack-v1", "color": "#9467bd"},
+    # "indoorhack-v2": {"name": "IndoorHack-v2", "color": "#8c564b"},
+    # "indoorhack-v21": {"name": "IndoorHack-v21", "color": "#e377c2"},
+
+    "innetvlad-v1-10": {"name": "InNetVLAD-v1-10", "color": "#9467bd", "linestyle": "dotted"},
+    "innetvlad-v1-20": {"name": "InNetVLAD-v1-20", "color": "#9467bd", "linestyle": "dashed"},
+    "innetvlad-v1-30": {"name": "InNetVLAD-v1-30", "color": "#9467bd", "linestyle": "solid"},
+
+    "innetvlad-v2-10": {"name": "InNetVLAD-v2-10", "color": "#8c564b", "linestyle": "dotted"},
+    "innetvlad-v2-20": {"name": "InNetVLAD-v2-20", "color": "#8c564b", "linestyle": "dashed"},
+    "innetvlad-v2-30": {"name": "InNetVLAD-v2-30", "color": "#8c564b", "linestyle": "solid"},
+
+    "innetvlad-v3-10": {"name": "InNetVLAD-v3-10", "color": "#e377c2", "linestyle": "dotted"},
+    "innetvlad-v3-20": {"name": "InNetVLAD-v3-20", "color": "#e377c2", "linestyle": "dashed"},
+    "innetvlad-v3-30": {"name": "InNetVLAD-v3-30", "color": "#e377c2", "linestyle": "solid"},
+
+    # "indoorhack-v2": {"name": "IndoorHack-v2", "color": "#8c564b"},
+    # "indoorhack-v21": {"name": "IndoorHack-v21", "color": "#e377c2"},
     # "indoorhack-v4": {"name": "IndoorHack-v4", "color": "#7f7f7f"},
     # "indoorhack-v5": {"name": "IndoorHack-v5", "color": "#bcbd22"},
     # "indoorhack-v6": {"name": "IndoorHack-v6", "color": "#17becf"},
