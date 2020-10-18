dbd5102d95bed4e936b1053ca44d3c4579b551b1
Filip Finfando <filip.finfando@gmail.com>
2020-10-18 17:08:07 +0000

    plot fix

--- indoorhack/tasks/plot_roc.py
+++ indoorhack/tasks/plot_roc.py
@@ -16,7 +16,7 @@ from config.plot import EXPERIMENT_MAP
 @click.option("--dataset_variant", help="Variant of a dataset.", multiple=True, required=False)
 @click.option(
     "--experiment_name",
-    type=click.Choice(["hash", "orb", "netvlad", "facenet", "indoorhack-v1", "indoorhack-v2", "indoorhack-v21"]),
+    # type=click.Choice(["hash", "orb", "netvlad", "facenet", "indoorhack-v1", "indoorhack-v2", "indoorhack-v21"]),
     multiple=True,
     required=True,
 )
@@ -35,9 +35,9 @@ def plot_roc(dataset_type, dataset_name, dataset_variant, experiment_name):
         distances = {}
         for e in experiment_name:
             if dataset_variant:
-                X_dist_path = main_path / f"{dataset_name}_{dataset_variant}_dist_{e}.npy"
+                X_dist_path = main_path / "distances" / f"{dataset_name}_{dataset_variant}_dist_{e}.npy"
             else:
-                X_dist_path = main_path / f"{dataset_name}_dist_{e}.npy"
+                X_dist_path = main_path / "distances" / f"{dataset_name}_dist_{e}.npy"
             distances[e] = np.load(X_dist_path)
 
         return distances, y
@@ -50,21 +50,21 @@ def plot_roc(dataset_type, dataset_name, dataset_variant, experiment_name):
 
         ax = plt.subplot(1, len(dataset_variant), i+1)
         for e, d in distances.items():
-            name, color = EXPERIMENT_MAP[e]["name"], EXPERIMENT_MAP[e]["color"]
+            name, color, linestyle = EXPERIMENT_MAP[e]["name"], EXPERIMENT_MAP[e]["color"], EXPERIMENT_MAP[e]["linestyle"]
             distances_array = np.array(d)
             distances_array_scaled = scale_fix(distances_array, 0, 1)
             score = 1 - distances_array_scaled
             fpr, tpr, thresholds = roc_curve(y, score)
-            plt.plot(fpr, tpr, label=name, linewidth=2, c=color)
+            plt.plot(fpr, tpr, label=name, linewidth=2, c=color, linestyle=linestyle)
 
         plt.plot([0, 1], [0, 1], c="blue", linewidth=0.5, linestyle="--")
         plt.grid()
         plt.xlabel("Fall-out", size=25)
         plt.ylabel("Recall", size=25)
-        plt.ylim((-0.05,1.05))
-        plt.xlim((-0.05,1.05))
-        # plt.ylim((.8, 1))
-        # plt.xlim((0, .2))
+        # plt.ylim((-0.05,1.05))
+        # plt.xlim((-0.05,1.05))
+        plt.ylim((.9, 1))
+        plt.xlim((0, .1))
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
         plt.legend(prop={'size': 30})
